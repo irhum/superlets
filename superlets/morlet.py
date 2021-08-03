0c3bd1c8891db0cef9af4fcc8d24cba47877572e
Irhum Shafkat <13271155+irhum@users.noreply.github.com>
2021-08-03 06:31:09 +0000

    push first demo

--- superlets/morlet.py
+++ superlets/morlet.py
@@ -7,20 +7,19 @@ import jax.numpy as jnp
 def get_bc(cycles, freq, k_sd=5):
   return cycles/(k_sd * freq)
 
-@partial(jax.jit, static_argnums=2)
-def cxmorelet(f, c, fs):
-  t = jnp.linspace(-1, 1, fs*2)
+def cxmorelet(freq, cycles, sampling_freq):
+    t = jnp.linspace(-1, 1, sampling_freq*2)
 
-  bc = get_bc(c, f)
-  norm = 1/(bc * jnp.sqrt(2*jnp.pi))
-  gauss = jnp.exp(-t**2/(2*bc**2))
-  sine = jnp.exp(1j*2*jnp.pi*f*t)
+    bc = get_bc(cycles, freq)
+    norm = 1/(bc * jnp.sqrt(2*jnp.pi))
+    gauss = jnp.exp(-t**2/(2*bc**2))
+    sine = jnp.exp(1j*2*jnp.pi*freq*t)
 
-  wavelet = norm * gauss * sine
-  return wavelet / jnp.sum(jnp.abs(wavelet))
+    wavelet = norm * gauss * sine
+    return wavelet / jnp.sum(jnp.abs(wavelet))
 
-@jax.jit
-@partial(jax.vmap, in_axes=(None, 0, None))
-def wavelet_transform(signal, f, c):
-  wavelet = cxmorelet(f, c, 1024)
-  return jax.scipy.signal.convolve(signal, wavelet, mode="same")
+@partial(jax.jit, static_argnums=3)
+@partial(jax.vmap, in_axes=(None, 0, None, None))
+def wavelet_transform(signal, freq, cycles, sampling_freq):    
+    wavelet = cxmorelet(freq, cycles, sampling_freq)
+    return jax.scipy.signal.convolve(signal, wavelet, mode="same")
--- superlets/superlets.py
+++ superlets/superlets.py
@@ -6,14 +6,17 @@ import jax.numpy as jnp
 from .morlet import wavelet_transform
 
 
-@jax.jit
-@partial(jax.vmap, in_axes=(None, None, 0))
-def superlet_transform_helper(signal, freqs, order):
-  return wavelet_transform(signal, freqs, order)
+@partial(jax.jit, static_argnums=3)
+@partial(jax.vmap, in_axes=(None, None, 0, None))
+def superlet_transform_helper(signal, freqs, order, sampling_freq):
+    return wavelet_transform(signal, freqs, order, sampling_freq) * jnp.sqrt(2)
 
-def order_to_cycles(base_cycle, max_order):
-    # return jnp.arange(1, max_order) * base_cycle
-    return jnp.arange(0, max_order) + base_cycle
+def order_to_cycles(base_cycle, max_order, mode):
+    if mode == "add":
+        return jnp.arange(0, max_order) + base_cycle
+    elif mode == "mul":
+        return jnp.arange(1, max_order+1) * base_cycle
+    else: raise ValueError("mode should be one of \"mul\" or \"add\"")
 
 def get_order(f, f_min: int, f_max: int, o_min: int, o_max: int):
     return o_min + round((o_max - o_min) * (f - f_min) / (f_max - f_min))
@@ -24,22 +27,17 @@ def get_mask(order, max_order):
 
 @jax.jit
 def norm_geomean(X, root_pows, eps):
-  X = jnp.log(X + eps).sum(axis=0)
+    X = jnp.log(X + eps).sum(axis=0)
 
-  return jnp.exp(X / jnp.array(root_pows).reshape(-1, 1))
+    return jnp.exp(X / jnp.array(root_pows).reshape(-1, 1))
 
 # @jax.jit 
-def superlet_transform(signal, freqs, base_cycle: int, min_order: int, max_order: int, eps=1e-12):
-  cycles = order_to_cycles(base_cycle, max_order)
-  orders = get_order(freqs, min(freqs), max(freqs), min_order, max_order)
+def adaptive_superlet_transform(signal, freqs, sampling_freq, base_cycle: int, min_order: int, max_order: int, eps=1e-12, mode="mul"):
+    cycles = order_to_cycles(base_cycle, max_order, mode)
+    orders = get_order(freqs, min(freqs), max(freqs), min_order, max_order)
+    mask = get_mask(orders, max_order)
 
-  out = superlet_transform_helper(signal, freqs, cycles)
+    out = superlet_transform_helper(signal, freqs, cycles, sampling_freq)
+    out = jax.ops.index_update(out, mask.T, 1)
 
-  mask = get_mask(orders, max_order)
-
-  out = jax.ops.index_update(out, mask.T, 1)
-
-  return norm_geomean(out, orders, eps)
-#   out = jnp.log(out + eps).sum(axis=0)
-
-#   return jnp.exp(out / jnp.array(orders).reshape(-1, 1))
+    return norm_geomean(out, orders, eps)
